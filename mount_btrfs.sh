8e3f8f5368cfce1595269830378c3d7ad0b1673b
Zeglius <33781398+Zeglius@users.noreply.github.com>
2025-07-19 00:37:56 +0200

    revert: withold multiple container-storage targets (#6)

--- mount_btrfs.sh
+++ mount_btrfs.sh
@@ -2,28 +2,17 @@
 
 set -eo pipefail
 
-readarray -t _BTRFS_TARGET_DIRS <<<"${BTRFS_TARGET_DIR:-$(
+BTRFS_TARGET_DIR="${BTRFS_TARGET_DIR:-$(
     dir=$(podman system info --format '{{.Store.GraphRoot}}' | sed 's|/storage$||')
-    sudo mkdir -p --mode 755 "$dir" && sudo chown "$(id -u)":"$(id -g)" "$dir"
-    sudo mkdir -p "/var/lib/containers/storage"
-    sudo mkdir -p "/var/lib/docker"
-
-    printf "%s\n" "$dir"
-    printf "%s\n" "/var/lib/containers/storage"
-    printf "%s\n" "/var/lib/docker"
-    echo ""
+    mkdir -p "$dir"
+    echo "$dir"
 )}"
-
-# Expand target directories
-readarray -t BTRFS_TARGET_DIRS < <(sudo realpath "${_BTRFS_TARGET_DIRS[@]}")
-unset -v _BTRFS_TARGET_DIRS
-
 # Options used to mount
 BTRFS_MOUNT_OPTS=${BTRFS_MOUNT_OPTS:-"compress-force=zstd:2"}
 # Location where the loopback file will be placed.
-_BTRFS_LOOPBACK_FILE=${_BTRFS_LOOPBACK_FILE:-/mnt/btrfs_loopbacks/shared_loopback}
+_BTRFS_LOOPBACK_FILE=${_BTRFS_LOOPBACK_FILE:-/mnt/btrfs_loopbacks/$(systemd-escape -p "$BTRFS_TARGET_DIR")}
 # Percentage of the total space to use. Max: 1.0, Min: 0.0
-BTRFS_LOOPBACK_FREE=${BTRFS_LOOPBACK_FREE:-"0.8"}
+BTRFS_LOOPBACK_FREE=${_BTRFS_LOOPBACK_FREE:-"0.8"}
 
 # Result of $(dirname "$_BTRFS_LOOPBACK_FILE")
 btrfs_pdir="$(dirname "$_BTRFS_LOOPBACK_FILE")"
@@ -41,25 +30,15 @@ _final_size=$(
 truncate -s "$_final_size" "$_BTRFS_LOOPBACK_FILE"
 unset -v _final_size
 
-# Format btrfs loopback
-sudo mkfs.btrfs -f "$_BTRFS_LOOPBACK_FILE"
+# # Stop docker services
+# sudo systemctl stop docker
 
-# Mount the loopback to a temporary directory
-_BTRFS_TEMP_MOUNT=$(mktemp -d)
-sudo systemd-mount "$_BTRFS_LOOPBACK_FILE" "$_BTRFS_TEMP_MOUNT" \
+# Format btrfs loopback
+sudo mkfs.btrfs -f -r "$BTRFS_TARGET_DIR" "$_BTRFS_LOOPBACK_FILE"
+
+# Mount
+sudo systemd-mount "$_BTRFS_LOOPBACK_FILE" "$BTRFS_TARGET_DIR" \
     ${BTRFS_MOUNT_OPTS:+ --options="${BTRFS_MOUNT_OPTS}"}
 
-for BTRFS_TARGET_DIR in "${BTRFS_TARGET_DIRS[@]}"; do
-    # Create a subvolume for each target directory
-    subvol="$_BTRFS_TEMP_MOUNT/${BTRFS_TARGET_DIR//\//-}"
-    sudo btrfs subvolume create "$subvol"
-    sudo chmod 755 "$subvol"
-
-    # Bind mount the subvolume to the target directory
-    sudo mkdir -p "$BTRFS_TARGET_DIR"
-    sudo mount --bind "$subvol" "$BTRFS_TARGET_DIR"
-done
-
-# Unmount the temporary directory
-sudo umount "$_BTRFS_TEMP_MOUNT"
-sudo rmdir "$_BTRFS_TEMP_MOUNT"
+# # Restart docker services
+# sudo systemctl start docker
